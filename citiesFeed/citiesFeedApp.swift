a39b5df823e41970375b3acd51370445af58782d
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-11-14 19:40:22 -0800

    Initial Commit

---
+++ citiesFeed/citiesFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  citiesFeedApp.swift
+//  citiesFeed
+//
+//  Created by Rosalie on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct citiesFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
